575957d1ab69582b641598223151a533700b996b
tinytom <to.man722@gmail.com>
2017-06-26 22:40:55 +0200

    Get/put

---
+++ fb_oauth/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        if todo_id not in todos.keys():
+            return 404
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
